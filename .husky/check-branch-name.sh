b0ceb54b72c26c2d928ff528d26880c050fd7ca8
Thomas Nguyen <thien.nguyen3@sotatek.com>
2022-05-19 12:10:41 +0700

    update

--- .husky/check-branch-name.sh
+++ .husky/check-branch-name.sh
@@ -2,12 +2,30 @@
 local_branch_name="$(git rev-parse --abbrev-ref HEAD)"
 
 develop_branch='develop'
-
-message="Denied commit and push to develop branch. Please use check out and create merge request"
-
+develop_message="Denied commit and push to develop branch. Please use check out and create merge request"
 if [[ $local_branch_name =~ $develop_branch ]]; then
   echo "*****************************"
-  echo "$message"
+  echo "$develop_message"
+  echo "*****************************"
+  exit 1
+fi
+
+
+master_branch='master'
+master_message="Denied commit and push to master branch. Please use check out and create merge request"
+if [[ $local_branch_name =~ $master_branch ]]; then
+  echo "*****************************"
+  echo "$master_message"
+  echo "*****************************"
+  exit 1
+fi
+
+
+production_branch='production'
+production_message="Denied commit and push to production branch. Please use check out and create merge request"
+if [[ $local_branch_name =~ $production_branch ]]; then
+  echo "*****************************"
+  echo "$production_message"
   echo "*****************************"
   exit 1
 fi
